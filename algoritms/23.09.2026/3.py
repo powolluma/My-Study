b479212b667408930fa6c1d40331bd41e4348df4
powolluma <sgdfgssdfgsergergsdfg@gmail.com>
2026-09-20 19:18:09 +0900

    Двумерные массивы на Python

---
+++ algoritms/23.09.2026/3.py
@@ -0,0 +1,47 @@
+import random
+
+def play_snake(n=10, m=10):
+    snake = [[5, 5], [5, 4], [5, 3]] 
+    direction = 'd'
+    
+    # Генерация яблока
+    def spawn_apple():
+        while True:
+            a = [random.randint(0, n-1), random.randint(0, m-1)]
+            if a not in snake: return a
+            
+    apple = spawn_apple()
+    opposites = {'w': 's', 's': 'w', 'a': 'd', 'd': 'a'}
+
+    while True:
+        # Рисуем поле
+        for r in range(n):
+            row_str = ""
+            for c in range(m):
+                if [r, c] == snake[0]: row_str += "O "      # Голова
+                elif [r, c] in snake: row_str += "o "       # Тело
+                elif [r, c] == apple: row_str += "@ "       # Яблоко
+                else: row_str += ". "
+            print(row_str)
+            
+        cmd = input("Ход (w/a/s/d): ").strip().lower()
+        if cmd in ['w', 'a', 's', 'd'] and cmd != opposites[direction]:
+            direction = cmd
+            
+        # Подчсчет новых координат головы
+        dr, dc = {'w':(-1,0), 's':(1,0), 'a':(0,-1), 'd':(0,1)}[direction]
+        new_head = [snake[0][0] + dr, snake[0][1] + dc]
+        
+        # Проверка столкновения со стеной или собой
+        if not (0 <= new_head[0] < n and 0 <= new_head[1] < m) or new_head in snake:
+            print("Игра окончена! Вы проиграли."); break
+            
+        snake.insert(0, new_head) # Новая голова
+        
+        # Проверка съедания яблока
+        if new_head == apple:
+            apple = spawn_apple()
+        else:
+            snake.pop() # Удаление хвоста, если яблоко не съедено
+
+play_snake()
---
+++ algoritms/23.09.2026/5.py
@@ -0,0 +1,54 @@
+import random
+
+def play_tanks():
+    # Карта 
+    field = [
+        ['#', '#', '#', '#', '#'],
+        ['#', 'P', '.', 'E', '#'],
+        ['#', '.', '#', '.', '#'],
+        ['#', '.', '.', '.', '#'],
+        ['#', '#', '#', '#', '#']
+    ]
+    px, py = 1, 1   # игрок
+    ex, ey = 1, 3   # враг
+    p_dir = (-1, 0) # направление вверх
+    
+    while True:
+        # Создание карты
+        for row in field: print(" ".join(row))
+        print("w/a/s/d - движение, f - выстрел.")
+        
+        cmd = input("Действие: ").strip().lower()
+        
+        # Движение и поворот игрока
+        if cmd in ['w', 'a', 's', 'd']:
+            dr, dc = {'w':(-1,0), 's':(1,0), 'a':(0,-1), 'd':(0,1)}[cmd]
+            p_dir = (dr, dc)
+            if field[px + dr][py + dc] == '.':
+                field[px][py] = '.'
+                px, py = px + dr, py + dc
+                field[px][py] = 'P'
+                
+        # Выстрел 
+        elif cmd == 'f':
+            bx, by = px + p_dir[0], py + p_dir[1]
+            while field[bx][by] == '.':
+                bx += p_dir[0]
+                by += p_dir[1]
+            if field[bx][by] == 'E':
+                print("Вы уничтожили врага! ПОБЕДА!")
+                break
+            else:
+                print("Промах (попали в стену).")
+                
+        # Cлучайное движение врага
+        if field[ex][ey] == 'E': # Если враг еще жив
+            edr, edc = random.choice([(-1,0), (1,0), (0,-1), (0,1)])
+            if field[ex + edr][ey + edc] == '.':
+                field[ex][ey] = '.'
+                ex, ey = ex + edr, ey + edc
+                field[ex][ey] = 'E'
+            elif field[ex + edr][ey + edc] == 'P':
+                print("Враг поймал вас! ПОРАЖЕНИЕ!"); break
+
+play_tanks()
